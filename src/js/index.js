d57f0fc9a56ff2471aa07799121031148e75c4a0
cmzluna <claudiomazzoli@gmail.com>
2021-05-13 19:11:12 -0300

    created AJAX calls using spoonacular API

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,23 @@
-// Global app controller
-import num from './test'; // 23 would be saved into the num variable
+import axios from 'axios'
 
-console.log(`I imported ${num} from another module`)
+// axios library to make AJAX requests
+async function getResults(query) {
+    const key = '0169652c09574bf5bd9bd3d9101aefec';
+/*
+    try {
+        const res = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}&includeNutrition=true&query=${query}&number=50`);
+        // this is going to return a promise
+        const recipes = res.data.results;
+        console.log(recipes);
+    } catch(error) {
+        alert('error');
+    }
+    */
+}
+
+getResults('pasta');
+
+// API KEY
+// 0169652c09574bf5bd9bd3d9101aefec 
+// https://api.spoonacular.com/recipes/complexSearch
+
